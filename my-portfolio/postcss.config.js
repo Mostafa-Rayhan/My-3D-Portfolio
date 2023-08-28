82c776c76f9da1ccf92c171a9f6cad56e81e7f91
mostafa-rayhan <mostafarayhan57@gmail.com>
2023-08-28 19:26:06 +0600

    complete components

---
+++ my-portfolio/postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// postcss.config.js
+// module.exports = {
+//   plugins: [
+//     require('tailwindcss'), // Make sure this line is present
+//     // Other PostCSS plugins...
+//   ],
+// };
